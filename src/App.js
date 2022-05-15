31100d4796fd46b57d1a17908593762221f2cd87
partho2589 <parthomalo615@.com>
2022-05-15 23:03:48 +0600

    added booking api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
+
+import { ToastContainer } from 'react-toastify';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
@@ -23,6 +25,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<SignUp />} />
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,18 +2,38 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
-
+import {  toast } from 'react-toastify';
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, slots } = treatment;
     const [user, loading, error] = useAuthState(auth);
-
+    const formattedDate = format(date, 'PP')
+        
     const handleBooking = event =>{
         event.preventDefault();
         const slot = event.target.slot.value;
         console.log(_id, name, slot);
-        
-        // to close the modal
-        setTreatment(null);
+        const booking={
+            treatmentId : _id,
+            treatment:name,
+            slot,
+            date:formattedDate,
+            patient:user.email,
+            patientName:user.display,
+            phone:event.target.value,
+        }
+        fetch('http://localhost:5000/booking',{
+            method:'POST',
+             headers:{
+                 'content-type':'application/json'
+             },
+             body: JSON.stringify(booking)
+        })
+        .then(res => res.json())
+        .then(date =>{
+            console.log(date)
+            // to close the modal
+            setTreatment(null);
+        })
     }
 
     return (
